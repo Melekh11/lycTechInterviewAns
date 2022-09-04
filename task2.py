366fa65893819216c8bd68ba61775276ac963a49
Melekh11 <cktkutxd@gmail.com>
2022-09-04 13:00:08 +0300

    comments

--- task2.py
+++ task2.py
@@ -3,7 +3,7 @@ from task2_ans import square  # ваша готовая функция
 #  напиать функцию, которая считает либо площадь либо объём шара
 #  функция может принять параметр is3d (False по умолчанию) и fix (int, 2 по умолчанию)
 
-print(square(3))
-print(square(3, fix=5))
-print(square(3, is3d=True, fix=3))
+print(square(3))  # 28.27
+print(square(3, fix=5))  # 28.27433
+print(square(3, is3d=True, fix=3))  # 113.097
 
